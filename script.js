7d11731c5979b962792ef5d55a020fb0321d474d
GeracleOmraldez <omaog2k9@gmail.com>
2025-02-25 10:07:15 +0800

    added images to fix

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.querySelector(".playlistButton").addEventListener("click", function() {
 
   playlistDiv.innerHTML = ""; // Clear previous content
   let img = document.createElement("img");
-  img.src = "https://cdn.discordapp.com/attachments/1198949087140126871/1336609430225158195/image.png?ex=67a46e09&is=67a31c89&hm=ed7ce7a029787a5f122e1709a6a478223b2c659452daf980ff7fcf6afcfea4f5&";
+  img.src = "images/myPlaylist.png";
   img.alt = "Playlist Image";
   img.classList.add("playlist-image");
 
