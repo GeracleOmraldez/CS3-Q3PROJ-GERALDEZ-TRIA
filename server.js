cd7e34414b8678a2d3677868d89988a80d04d598
GeracleOmraldez <omaog2k9@gmail.com>
2025-05-09 22:28:07 +0800

    i am sleeping good tonight

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -23,6 +27,43 @@ app.get('/gabgab', (req, res) => res.render('gabgab'));
 app.get('/oracle', (req, res) => res.render('oracle'));
 app.get('/myPlaylist', (req, res) => res.render('myPlaylist'));
 app.get('/statistics', (req, res) => res.render('statistics'));
+
+app.post('/create-account', (req, res) => {
+  const newAccount = {
+    fullName: req.body.fullname,
+    username: req.body.username,
+    birthday: req.body.birthday,
+    email: req.body.email,
+    age: Number(req.body.age),
+    musicGenre: req.body.musicgenre,
+    musicArtists: req.body.musicartist
+  };
+
+  app.post('/delete-account', (req, res) => {
+  const { username } = req.body;
+  const filePath = path.join(__dirname, 'data', 'users.json');
+  const rawData = fs.readFileSync(filePath);
+  const jsonData = JSON.parse(rawData);
+
+  jsonData.accounts = jsonData.accounts.filter(account => account.username !== username);
+
+  fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+
+  res.redirect('/');
+});
+
+
+  const filePath = path.join(__dirname, 'data', 'users.json');
+  const rawData = fs.readFileSync(filePath);
+  const jsonData = JSON.parse(rawData);
+
+  jsonData.accounts.push(newAccount);
+
+  fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+
+  res.render('results', newAccount);
+});
+
 
 // FORM SUBMISSION
 app.post('/submit', (req, res) => {
